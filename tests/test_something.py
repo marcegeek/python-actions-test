49e919e82181b7e2e6c7a735345098f081fd40e4
Marcelo Castellano <marce.geek22@gmail.com>
2024-08-10 19:47:10 -0300

    Write a failing test

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,6 +1,12 @@
+import pytest
+
 import mymodule
 
 
 def test_somefunction_42():
     ret = mymodule.somefunction()
     assert ret == 42
+
+
+def test_fail():
+    pytest.fail("Failing on purpose")
